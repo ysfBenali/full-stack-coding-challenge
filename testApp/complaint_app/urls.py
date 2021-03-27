f7dd3bcd7930099b7158198e60e8d6e4e0c5df4d
Youssef BenAli <youyssef@gmail.com>
2021-03-27 19:17:38 +0100

    creating MVP 1 endpoints

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -4,9 +4,9 @@ from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopCo
 
 router = routers.SimpleRouter()
 router.register(r'', ComplaintViewSet, base_name='complaint')
-router.register(r'openCases', OpenCasesViewSet, base_name='openCases')
-router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases')
-router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
+router.register(r'/openCases', OpenCasesViewSet, base_name='openCases')
+router.register(r'/closedCases', ClosedCasesViewSet, base_name='closedCases')
+router.register(r'/topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
 urlpatterns = [
 ]
 urlpatterns += router.urls
--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -8,32 +8,59 @@ from rest_framework import status
 
 class ComplaintViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
-    serializer_class = ComplaintSerializer
 
+    queryset = Complaint.objects.all()
+    serializer_class = ComplaintSerializer(queryset, many=True)
+    
     def list(self, request):
         # Get all complaints from the user's district
-        return Response()
+        return Response(self.serializer_class.data)
 
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
-
+    
+    queryset = Complaint.objects.filter(closedate__isnull=True)
+    serializer_class = ComplaintSerializer(queryset, many=True)
+    
     def list(self, request):
         # Get only the open complaints from the user's district
-        return Response()
+        return Response(self.serializer_class.data)
 
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
 
+    queryset = Complaint.objects.filter(closedate__isnull=False)
+    serializer_class = ComplaintSerializer(queryset, many=True)
+    
     def list(self, request):
         # Get only complaints that are close from the user's district
-        return Response()
+        return Response(self.serializer_class.data)
 
 
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
 
+    # Dict to store complaint types & account district
+    complaintTypesByCode = {}
+
+    # Get non null complaint types & account district
+    complaints = Complaint.objects.values('complaint_type','account').filter(complaint_type__isnull=False)
+    for one_complaint in complaints :
+        if one_complaint['account'] not in complaintTypesByCode :
+            complaintTypesByCode[one_complaint['account']] = {one_complaint['complaint_type']: 1}
+        else:
+            if one_complaint['complaint_type'] in complaintTypesByCode[one_complaint['account']]:
+                complaintTypesByCode[one_complaint['account']][one_complaint['complaint_type']] += 1
+            else:
+                 complaintTypesByCode[one_complaint['account']][one_complaint['complaint_type']] = 1
+   
+    # Sorting in descending order, the complaint types for each district
+    for one_district in complaintTypesByCode:
+        complaintTypesByCode[one_district] = sorted(complaintTypesByCode[one_district].items(), key = lambda k:(k[1], k[0]), reverse=True)
+
     def list(self, request):
         # Get the top 3 complaint types from the user's district
-        return Response()
+        return Response(self.complaintTypesByCode)
+
