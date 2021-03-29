40f5d5b3fd410631d9f9e84117e24b7e3147a864
Youssef BenAli <youyssef@gmail.com>
2021-03-30 00:18:25 +0100

    style: login page

--- testApp/frontend/src/components/auth/Login.jsx
+++ testApp/frontend/src/components/auth/Login.jsx
@@ -5,6 +5,7 @@ import axiosInstance from "../../helpers/axios";
 
 const Login = () => {
   const [credentials, handleChange] = useForm({ username: "", password: "" });
+  const [isError, setIsError] = useState(false);
   let history = useHistory();
 
   const handleSubmit = (event) => {
@@ -16,27 +17,94 @@ const Login = () => {
         localStorage.setItem("token", token);
         history.push(`/dashboard`);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setIsError(true);
+        console.error(err);
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        value={credentials.username}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="password"
-        placeholder="Password"
-        value={credentials.password}
-        onChange={handleChange}
-      />
-      <button>Login</button>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <svg
+            className="mx-auto h-16 w-auto text-indigo-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div className="mb-4">
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                style={{ borderColor: isError ? "red" : "" }}
+                placeholder="Username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                style={{ borderColor: isError ? "red" : "" }}
+                placeholder="Password"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <span className="mt-2 text-red-500">
+            {isError ? "Incorrect login and password !" : ""}
+          </span>
+          <div>
+            <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <svg
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
