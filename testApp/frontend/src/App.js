e90e1f9e6184fcae4d0a6264e09646e10208e12a
Youssef BenAli <youyssef@gmail.com>
2021-04-03 14:40:14 +0100

    chore: react-query setup

--- testApp/frontend/src/App.js
+++ testApp/frontend/src/App.js
@@ -3,23 +3,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NavBar from "./components/NavBar";
 import NotFound from "./pages/NotFound";
-import Dashboard from "./components/Dashboard";
+import AllComplaints from "./components/AllComplaints";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/login" component={Login} />
-          <Route default component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={AllComplaints} />
+            <Route path="/all" component={AllComplaints} />
+            <Route path="/login" component={Login} />
+            <Route default component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
