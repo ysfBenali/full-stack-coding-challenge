1803bd49608b12dd3b2163aedae7fc39988b1add
Youssef BenAli <youyssef@gmail.com>
2021-04-04 18:55:32 +0100

    adding Alert comps

---
+++ testApp/frontend/src/components/Alert/ErrorFetch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorFetch = () => {
+  return <div className="flex justify-center">Error fetching data ...</div>;
+};
+
+export default ErrorFetch;
---
+++ testApp/frontend/src/components/Alert/Loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div className="flex justify-center">loading data ...</div>;
+};
+
+export default Loading;
